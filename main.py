0f5507f3ea3d39538b02342345227f39fae1e25f
skw1234567890 <charlie.shikewei@gmail.com>
2015-03-30 18:27:29 -0400

    add sample

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ from CatchData import *
 from FetchData import *
 from DataType import *
 
+from SampleModel import *
+
 if __name__ == "__main__":
 #	print(StockList.getAll())
 #	print(FetchData.fetchByNameAndPeriodUpToNowByType("AAPL", 50, "Close"))
 #	StockList.addStock("GS")
 #	StockList.doAll(CatchData.catchByName, None)
 #	CatchData.catchByName("V")
-#	for e in StockList.doAll(FetchData.fetchByNameAndPeriodUpToNowByType, (100, "Open")):
-#		print e
+	for e in StockList.doAll(SampleModel.avgOfHistoryPriceRatioPrinter, (200, 50, "Close")):
+		print e
+	print("")
+	for e in StockList.doAll(SampleModel.avgOfHistoryPriceRatioPrinter, (200, 50, "Open")):
+		print e
--- models/SampleModel.py
+++ models/SampleModel.py
@@ -1,8 +1,36 @@
+#
+# SampleModel class:
+#	method:
+#		avgOfHistoryPrice
+#		avgOfHistoryPriceRatio
+#		avgOfHistoryPriceRatioPrinter
+#
+
+import sys
+sys.path.append("./tools")
+
+from FetchData import *
+
 ROOT_DIRECTORY = "./"
 DATA_DIRECTORY = ROOT_DIRECTORY + "./datas/"
 
-
 class SampleModel:
 	@staticmethod 
-	def avgOfHistoryPrice(stock, days, dataType):
-		None
+	def avgOfHistoryPrice(stockname, days, dataType):
+		result = FetchData.fetchByNameAndPeriodUpToNowByType(stockname, days, dataType)
+		total = 0
+		for e in result:
+			total += float(e)
+		return total/len(result)
+
+	@staticmethod
+	def avgOfHistoryPriceRatio(stockname, longDays, shortDays, dataType):
+		longAvg = SampleModel.avgOfHistoryPrice(stockname, longDays, dataType)
+		shortAvg = SampleModel.avgOfHistoryPrice(stockname, shortDays, dataType)
+		ratio = float(float(shortAvg) - float(longAvg))/float(longAvg)
+		return longAvg, shortAvg, ratio
+
+	@staticmethod
+	def avgOfHistoryPriceRatioPrinter(stockname, longDays, shortDays, dataType):
+		longAvg, shortAvg, ratio = SampleModel.avgOfHistoryPriceRatio(stockname, longDays, shortDays, dataType)
+		return dataType + "::" + str(longDays) + "days Avg: " + str(longAvg) + "    " + str(shortDays) + "days Avg: " + str(shortAvg) + "    ratio: " + str(ratio)
